d606b833120fe9639d8ac5b504e6aaa3c88f3742
Nathan Urbain <nathan.urbain@etu.unistra.fr>
2017-04-24 22:50:41 +0200

    Interface Serveur + btn menu fonctionnelle

--- Projet2IHM/mainwindow.cpp
+++ Projet2IHM/mainwindow.cpp
@@ -12,3 +12,34 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_BtnMenu_Music_clicked()
+{
+    ui->TabInteraction->setCurrentIndex(0);
+}
+
+void MainWindow::on_BtnMenu_Radio_clicked()
+{
+    ui->TabInteraction->setCurrentIndex(1);}
+
+void MainWindow::on_BtnMenu_serveur_clicked()
+{
+    ui->TabInteraction->setCurrentIndex(2);
+}
+
+void MainWindow::on_BtnMenu_Param_clicked()
+{
+    ui->TabInteraction->setCurrentIndex(3);
+}
+
+void MainWindow::on_BtnMenu_Play_clicked()
+{
+    ui->TabInteraction->setCurrentIndex(4);
+}
+
+void MainWindow::on_BtnMenu_Attente_clicked()
+{
+     ui->TabInteraction->setCurrentIndex(5);
+}
+
+
--- Projet2IHM/mainwindow.h
+++ Projet2IHM/mainwindow.h
@@ -15,6 +15,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_BtnMenu_Music_clicked();
+
+    void on_BtnMenu_Radio_clicked();
+
+    void on_BtnMenu_serveur_clicked();
+
+    void on_BtnMenu_Param_clicked();
+
+    void on_BtnMenu_Attente_clicked();
+
+    void on_BtnMenu_Play_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
